203afe7e1f5fdd977d595936d4bcc8c2f05675e1
Maher Deeb <maherdeebcv@gmail.com>
2018-10-29 23:26:31 +0100

    clean the preprossing to change only the string to integers'

--- preprossesing_data.py
+++ preprossesing_data.py
@@ -8,26 +8,10 @@ import pandas as pd
 
 data_path = './data/'
 
-df_train = pd.read_csv('{}train_expanded.csv'.format(data_path), engine='python')
-df_test = pd.read_csv('{}test_expanded.csv'.format(data_path), engine='python')
-
-expected_column_to_detet=['socialEngagementType','browserSize','browserVersion','flashVersion',
-                         'language','mobileDeviceBranding','mobileDeviceInfo','mobileDeviceMarketingName',
-                         'mobileDeviceModel','mobileInputSelector','operatingSystemVersion','screenColors',
-                          'screenResolution','cityId','latitude','longitude','networkLocation','visits',
-                          'campaignCode','criteriaParameters','targetingCriteria']
-
-def drop_unnecessary_columns(expected_column_to_detet,df_train,df_test):
-    for col in expected_column_to_detet:
-        df_train = df_train.drop([col],axis = 1)
-    for col in expected_column_to_detet:
-        try:
-            df_test = df_test.drop([col],axis = 1)
-        except:
-            print('Column {} is not exiting in the test dataset'.format(col))
-    return df_train, df_test
-
-df_train, df_test = drop_unnecessary_columns(expected_column_to_detet,df_train,df_test)
+df_train = pd.read_csv('{}trainset_without_unncessarycols.csv'.\
+                       format(data_path), engine='python')
+df_test = pd.read_csv('{}testset_without_unncessarycols.csv'.\
+                      format(data_path), engine='python')
 
 Y_train = df_train.transactionRevenue
 df_train = df_train.drop(['transactionRevenue'],axis = 1)
@@ -55,4 +39,5 @@ df_train, df_test = replace_strings_integer(df_train, df_test)
 df_train.to_csv('{}train_clean.csv'.format(data_path),index = False)
 df_test.to_csv('{}test_clean.csv'.format(data_path),index = False)
 Y_train.to_csv('{}target_clean.csv'.format(data_path),index = False)
-fullVisitorId_test.to_csv('{}test_id_clean.csv'.format(data_path),index = False)
+fullVisitorId_test.to_csv('{}test_id_clean.csv'.\
+                          format(data_path),index = False)
