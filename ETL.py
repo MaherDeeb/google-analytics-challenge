8e10d85c7569da1b2ade2aed6ab26db91263b300
Maher Deeb <maherdeebcv@gmail.com>
2018-11-10 17:24:52 +0100

    ETL tha fit the new dataset without hits

--- ETL.py
+++ ETL.py
@@ -26,20 +26,37 @@ def separate_json(series: pd.Series) -> pd.DataFrame():
         return pd.DataFrame(json.loads(s) for s in series)
     return pd.DataFrame(s for s in series)
 
-json_col = ['device', 'geoNetwork', 'totals', 'trafficSource']
-nest_json_col = ['adwordsClickInfo']
+def apply_sepration(df_org):
 
-df_train = pd.read_csv('{}train.csv'.format(data_path), engine='python')
-df_train = df_train.join(separate_json(
-    df_train[col_name]) for col_name in json_col).drop(json_col, axis=1)
-df_train = df_train.join(separate_json(
-    df_train[nest_json_col[0]])).drop(nest_json_col, axis=1)
+    json_col = ['customDimensions','device', 'geoNetwork', 'totals', 'trafficSource',
+                'trafficSource.adwordsClickInfo','customDimensions.0',
+               'trafficSource.adwordsClickInfo.targetingCriteria']
+    for col_name in json_col:
+        if col_name == 'customDimensions.0':
+            df_org[col_name]=df_org[col_name].\
+            map(lambda x: {'index':'None'} if x is None else x)
 
-df_test = pd.read_csv('{}test.csv'.format(data_path), engine='python')
-df_test = df_test.join(separate_json(
-    df_test[col_name]) for col_name in json_col).drop(json_col, axis=1)
-df_test = df_test.join(separate_json(
-    df_test[nest_json_col[0]])).drop(nest_json_col, axis=1)
+        df = separate_json(df_train[col_name])
+        df.columns = ['{}.{}'.format(col_name,x) for x in list(df.columns)]
+    
+        df_org = df_org.join(df)
+        df_org = df_org.drop(col_name,axis=1)
+
+    return df_org
+
+nest_json_col = ['hits']
+df_train = pd.read_csv('{}train_v2.csv'.format(data_path) ,nrows=20000,engine='python')
+df_train = df_train.drop('hits',axis=1)
+df_train ['customDimensions']= df_train['customDimensions'].map(lambda x: str(x).replace("\'", "\""))
+
+df_train = apply_sepration(df_train)
+#df_train.columns
+
+df_test = pd.read_csv('{}test_v2.csv'.format(data_path),nrows=20000,engine='python')
+df_test ['customDimensions']= df_test['customDimensions'].map(lambda x: str(x).replace("\'", "\""))
+df_test = df_test.drop('hits',axis=1)
+
+df_test = apply_sepration(df_test)
 
 df_train.to_csv('{}train_expanded.csv'.format(data_path),index = False)
 df_test.to_csv('{}test_expanded.csv'.format(data_path),index = False)
