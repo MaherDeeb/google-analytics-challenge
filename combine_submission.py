66062205b0f2ed4c49ee842fc9bdb9c1e03383a5
Maher Deeb <maherdeebcv@gmail.com>
2018-11-15 21:25:23 +0100

    combine submission maher anjana

--- combine_submission.py
+++ combine_submission.py
@@ -8,13 +8,14 @@ Created on Sun Jun 24 22:02:44 2018
 import pandas as pd
 import datetime, time
 
-df_best_submit1 = pd.read_csv('1530501616_submit.csv')
-df_best_submit2 = pd.read_csv('1530502716_submit.csv')
+path = './data/'
+df_best_submit1 = pd.read_csv('{}1542066730_submit.csv'.format(path))
+df_best_submit2 = pd.read_csv('{}anjana_best_score_submission.csv'.format(path))
 
 df_best_submit = df_best_submit1
 
-df_best_submit['TARGET'] = df_best_submit['TARGET']+df_best_submit2['TARGET']
-df_best_submit['TARGET'] = df_best_submit['TARGET']/2
+df_best_submit['PredictedLogRevenue'] = df_best_submit['PredictedLogRevenue']+df_best_submit2['PredictedLogRevenue']
+df_best_submit['PredictedLogRevenue'] = df_best_submit['PredictedLogRevenue']/2
 
-df_best_submit.to_csv('{}_submit.csv'.format(str(round(time.mktime((datetime.datetime.now().timetuple()))))),index=False)
+df_best_submit.to_csv('{}{}_submit.csv'.format(path,str(round(time.mktime((datetime.datetime.now().timetuple()))))),index=False)
 
