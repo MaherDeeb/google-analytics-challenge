ebb91eda521580a83b64b0f377d35b1f04e61f93
Maher Deeb <maherdeebcv@gmail.com>
2018-11-17 14:33:48 +0100

    combine model with weights

--- combine_submission.py
+++ combine_submission.py
@@ -9,13 +9,13 @@ import pandas as pd
 import datetime, time
 
 path = './data/'
-df_best_submit1 = pd.read_csv('{}1542066730_submit.csv'.format(path))
-df_best_submit2 = pd.read_csv('{}anjana_best_score_submission.csv'.format(path))
+df_best_submit1 = pd.read_csv('{}1542461280_submit.csv'.format(path))
+df_best_submit2 = pd.read_csv('{}1542402842_submit.csv'.format(path))
 
 df_best_submit = df_best_submit1
 
-df_best_submit['PredictedLogRevenue'] = df_best_submit['PredictedLogRevenue']+df_best_submit2['PredictedLogRevenue']
-df_best_submit['PredictedLogRevenue'] = df_best_submit['PredictedLogRevenue']/2
+df_best_submit['PredictedLogRevenue'] = (1/1.6)*df_best_submit['PredictedLogRevenue']+(1/0.8)*df_best_submit2['PredictedLogRevenue']
+df_best_submit['PredictedLogRevenue'] = df_best_submit['PredictedLogRevenue']/((1/1.6)+(1/0.8))
 
 df_best_submit.to_csv('{}{}_submit.csv'.format(path,str(round(time.mktime((datetime.datetime.now().timetuple()))))),index=False)
 
